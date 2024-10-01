db8e4eb90a00e71ca21ec71ea65dad4b0b2b8d63
tudemaha <gede_mahardika88@yahoo.co.id>
2024-10-01 15:20:08 +0800

    fix: add condition to load .env

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-	_ "github.com/joho/godotenv/autoload"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/tudemaha/toode/pkg/mongodb"
 	"github.com/tudemaha/toode/pkg/server"
 )
 
 func main() {
+	if os.Getenv("LOAD_DOTENV") == "yes" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatalf("ERROR: Load loading .env file")
+		}
+	}
+
 	mongodb.StartConnection()
 	server.StartServer()
 }
